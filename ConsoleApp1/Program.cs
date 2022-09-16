8d0e0e433294c50d0546a5289232511e77be7b5a
Denys <navrotskyidenys@gmail.com>
2022-09-16 14:27:31 +0300

    Add VS project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RangeExtraction.Extract(new[] { 1, 2, 3 });
+        }
+    }
+
+    public class RangeExtraction
+    {
+        public static string Extract(int[] args)
+        {
+            string result = "";
+
+            for (var i = 0; i < args.Length; i++)
+            {
+                List<int> seq = new List<int> { args[i] };
+                while (i < args.Length - 1 && args[i + 1] == args[i] + 1)
+                    seq.Add(args[++i]);
+                if (seq.Count < 3)
+                    result = seq.Aggregate(result, (s, n) => s + (n + ","));
+                else
+                    result += seq[0] + "-" + seq[seq.Count-1] + ",";
+            }
+            return result.TrimEnd(',');
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class RangeExtraction
+{
+    public static string Extract(int[] args)
+    {
+        string result = "";
+
+        for (var i = 0; i < args.Length; i++)
+        {
+            List<int> seq = new List<int> { args[i] };
+            while (i < args.Length - 1 && args[i + 1] == args[i] + 1)
+                seq.Add(args[++i]);
+            if (seq.Count < 3)
+                result = seq.Aggregate(result, (s, n) => s + (n + ","));
+            else
+                result += seq[0] + "-" + seq[^1] + ",";
+        }
+        return result.TrimEnd(',');
+    }
+}
